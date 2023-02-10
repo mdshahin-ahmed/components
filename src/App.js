dadd4477973ce7e4f9ea2849fcfc792f1ffcc7fb
shahinahmed6057 <mdshahinahmed605701@gmail.com>
2023-02-10 11:12:58 +0600

    Created dropdown page

--- src/App.js
+++ src/App.js
@@ -1,23 +1,7 @@
-import React, { useState } from "react";
-import DropDown from "./components/DropDown";
-
-const options = [
-  { label: "Red", value: "red" },
-  { label: "Green", value: "green" },
-  { label: "Blue", value: "blue" },
-];
+import React from "react";
 
 const App = () => {
-  const [selection, setSelection] = useState(null);
-
-  const handleSelect = (option) => {
-    setSelection(option);
-  };
-  return (
-    <>
-      <DropDown onChange={handleSelect} value={selection} options={options} />
-    </>
-  );
+  return <></>;
 };
 
 export default App;
---
+++ src/pages/DropdownPage.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+import DropDown from "../components/DropDown";
+
+const options = [
+  { label: "Red", value: "red" },
+  { label: "Green", value: "green" },
+  { label: "Blue", value: "blue" },
+];
+
+const DropdownPage = () => {
+  const [selection, setSelection] = useState(null);
+
+  const handleSelect = (option) => {
+    setSelection(option);
+  };
+  return (
+    <>
+      <DropDown onChange={handleSelect} value={selection} options={options} />
+    </>
+  );
+};
+
+export default DropdownPage;
