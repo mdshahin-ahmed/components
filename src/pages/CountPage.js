211c7dd34feea05a5dedc808d8e9f69fbc192445
shahinahmed6057 <mdshahinahmed605701@gmail.com>
2023-02-13 12:31:02 +0600

    Added immer

--- src/pages/CountPage.js
+++ src/pages/CountPage.js
@@ -10,41 +10,30 @@ const ADD_VALUE_TO_COUNT = "ADD_VALUE_TO_COUNT";
 const reducer = (state, action) => {
   switch (action.type) {
     case INCREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count + 1,
-      };
+      state.count = state.count + 1;
+      return;
 
     case DECREMENT_COUNT:
-      return {
-        ...state,
-        count: state.count - 1,
-      };
+      state.count = state.count - 1;
+      return;
     case SET_VALUE_TO_ADD:
-      return {
-        ...state,
-        valueToAdd: action.payload,
-      };
+      state.valueToAdd = action.payload;
+      return;
+
     case ADD_VALUE_TO_COUNT:
-      return {
-        ...state,
-        count: state.count + state.valueToAdd,
-        valueToAdd: 0,
-      };
+      state.count = state.count + state.valueToAdd;
+      state.valueToAdd = 0;
+      return;
 
     default:
-      return state;
+      return;
   }
 };
 
 const CountPage = ({ initialCount }) => {
-  // const [count, setCount] = useState(initialCount);
-  // const [valueToAdd, setValueToAdd] = useState(0);
-
   const [state, dispatch] = useReducer(reducer, {
     count: initialCount,
     valueToAdd: 0,
-    something: true,
   });
 
   console.log(state);
